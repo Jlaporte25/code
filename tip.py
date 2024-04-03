03574c38d7c52b2786c3383e4a64544cc9caef1e
Jacob <you@example.com>
2024-04-03 03:10:46 -0500

    test

--- tip.py
+++ tip.py
@@ -1,17 +1,15 @@
 def main():
-    dollars = dollars_to_float(input("How much was the meal? "))
-    percent = percent_to_float(input("What percentage would you like to tip? "))
+    dollars = dollars_to_float(input("How much was the meal? ").strip("$"))
+    percent = percent_to_float(input("What percentage would you like to tip? ").strip("%"))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
 
 def dollars_to_float(d):
-    # TODO
-
+    return float(d)
 
 
 def percent_to_float(p):
-    # TODO
-
+    return float(p) / 100
 
 main()
