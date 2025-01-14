8e3f0a225b043a8e07dc9d3fb5dfcd1444ca47c0
Jlaporte25 <jacoblaporte25@gmail.com>
2025-01-13 20:35:00 -0600

    changed navbar

--- web_programming/project4/network/views.py
+++ web_programming/project4/network/views.py
@@ -89,3 +89,100 @@ def profile(request, username=None):
         "user": user,
         "posts": posts
     })
+
+@login_required
+def following(request):
+    user = request.user
+    following = user.following.all()
+    posts = Post.objects.filter(user__in=following).order_by("-timestamp")
+    return render(request, "network/following.html", {
+        "posts": posts
+    })
+
+@login_required
+def follow(request, username):
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return render(request, "network/profile.html", {
+            "message": "User not found."
+        })
+    
+    request.user.following.add(user)
+    return HttpResponseRedirect(reverse("profile", args=[username]))
+
+@login_required
+def unfollow(request, username):
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return render(request, "network/profile.html", {
+            "message": "User not found."
+        })
+    
+    request.user.following.remove(user)
+    return HttpResponseRedirect(reverse("profile", args=[username]))
+
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        content = request.POST["content"]
+        post = Post(user=request.user, content=content)
+        post.save()
+        return HttpResponseRedirect(reverse("all_posts"))
+    else:
+        return render(request, "network/create_post.html")
+
+@login_required
+def edit_post(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        return render(request, "network/all_posts.html", {
+            "message": "Post not found."
+        })
+    
+    if request.method == "POST":
+        post.content = request.POST["content"]
+        post.save()
+        return HttpResponseRedirect(reverse("all_posts"))
+    else:
+        return render(request, "network/edit_post.html", {
+            "post": post
+        })
+
+@login_required
+def like(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        return render(request, "network/all_posts.html", {
+            "message": "Post not found."
+        })
+    
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse("all_posts"))
+
+@login_required
+def unlike(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        return render(request, "network/all_posts.html", {
+            "message": "Post not found."
+        })
+    
+    post.likes.remove(request.user)
+    return HttpResponseRedirect(reverse("all_posts"))
+
+@login_required
+def delete_post(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        return render(request, "network/all_posts.html", {
+            "message": "Post not found."
+        })
+    
+    post.delete()
+    return HttpResponseRedirect(reverse("all_posts"))
