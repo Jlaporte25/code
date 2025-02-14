b16f3339ec7bc3009da924172073586db0032978
Jacob <you@example.com>
2025-02-14 15:52:22 -0600

    format

--- free_code_camp/intro_algo_data_struct/selection_sort/selection_sort.py
+++ free_code_camp/intro_algo_data_struct/selection_sort/selection_sort.py
@@ -1,5 +1,5 @@
+numbers = [5, 1, 8, 6, 2, 9]
 
-numbers = [5,1,8,6,2,9]
 
 def selection_sort(values):
     sorted_list = []
@@ -9,11 +9,13 @@ def selection_sort(values):
         sorted_list.append(values.pop(index_to_move))
     return sorted_list
 
+
 def index_of_min(values):
     min_index = 0
     for i in range(1, len(values)):
         if values[i] < values[min_index]:
             min_index = i
     return min_index
+
 
 print(selection_sort(numbers))
